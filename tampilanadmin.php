ba8f878bc512460082da2cb5c443283c8d0f3b23
Achmad Fadli Iskandar <68363467+achmadfadliiskandar@users.noreply.github.com>
2021-03-25 12:42:31 +0700

    Add files via upload

---
+++ tampilanadmin.php
@@ -0,0 +1,62 @@
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Log in</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="AdminLTE-3.1.0/plugins/fontawesome-free/css/all.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="AdminLTE-3.1.0/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="AdminLTE-3.1.0/dist/css/adminlte.min.css">
+  </head>
+  <body class="hold-transition login-page">
+  <div class="login-box">
+    <div class="login-logo">
+      <a href="../../index2.html"><b>Admin</b>LTE</a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="card">
+      <div class="card-body login-card-body">
+        <p class="login-box-msg">Login Khusus Admin</p>
+
+        <form action="login.php" method="POST">
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Isi User Name"  name="username">
+            <div class="input-group-append">
+              <div class="input-group-text">
+              <span class="fas fa-user-alt"></span>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mb-3">
+            <input type="password" class="form-control" placeholder="Password" name="password">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+        </form>
+
+        <?php
+        if (isset($_GET['pesan'])) {?>
+       <p><?php  echo $_GET['pesan'] ?></p>
+        }
+        <?php } ?>
+
+  <!-- jQuery -->
+  <script src="AdminLTE-3.1.0/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="AdminLTE-3.1.0/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="AdminLTE-3.1.0/dist/js/adminlte.min.js"></script>
+  </body>
+  </html>
