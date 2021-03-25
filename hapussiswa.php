d71795ff161ebc34c6220d10b52671486d709e22
Achmad Fadli Iskandar <68363467+achmadfadliiskandar@users.noreply.github.com>
2021-03-25 12:30:41 +0700

    Update hapussiswa.php

--- hapussiswa.php
+++ hapussiswa.php
@@ -10,7 +10,7 @@ $sql = "DELETE FROM siswa WHERE id='$id'";
 $query = mysqli_query($connect, $sql);
 
 if ($query) {
-    header('Location:tampilsiswa.php');
+    header('Location:halaman_admin.php');
 }else {
     header('Location:hapussiwa.php?status=gagal');
 }
